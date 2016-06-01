a868ab357a24d2fe812d8c3f2aad7ed6de0af014
juliano <juliano@JulianoDebian.home>
2016-06-01 20:59:43 -0300

    simula e desenho para quadrado funcional.

--- EP4/ep4.py
+++ EP4/ep4.py
@@ -2,16 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as r 
 def main():
-	tipo, listaVivas = leEntrada("arquivo.txt")	
-	nova = simulaQuad(10,10,listaVivas,1)	
+	n = m = 25
+	n = 17
+	t = 9
+	tipo, listaVivas = leEntrada("arquivo.txt")
 	
-	print(listaVivas)
-	print(type(listaVivas))
-	n = m = 10
-	print(nova)
+	nova = simulaQuad(n,m,listaVivas,t)	
 
-	print(geraMatriz(m,n,listaVivas))
-	desenhaQuad(n,m,listaVivas,0)
+	matriz = geraMatriz2(n,m,nova)
+	
+	for x in range(n):
+		for j in range(m):
+			print(matriz[x][j],end="")
+			print(" | ",end="")
+		print("\n")
+
+	desenhaQuad(n,m,nova,"nome")
 
 def geraMatriz(n,m,listaVivas):	
 	dim = (n,m)
@@ -19,56 +25,74 @@ def geraMatriz(n,m,listaVivas):
 	vivas = list(listaVivas)
 	for cel in vivas:
 		matriz[cel[0]][cel[1]] = 1
-		#valorVizinhos = sum([matriz[item[0]][item[1]] for item in vizinhosComuns])
-	matriz = np.flipud(matriz)
 	return matriz
 
+def geraMatriz2(n,m,listaVivas):	
+	dim = (n,m)
+	matriz = []
+	for i in range(n):
+		matriz.append([])
+		for j in range(m):
+			matriz[i]+=[" "]
+	vivas = list(listaVivas)
+	for cel in vivas:
+		matriz[cel[0]][cel[1]] = "X"
+	return matriz
 
 
 	#print(p)
 	#print(q)
 	
+def calculaVizinhos(cel,matriz,n,m,cord = False):
+	x,y = cel[0],cel[1]
+	vizinhosComuns =[
+			((x+1)%n,(y-1)%m),
+			((x+1)%n,y),
+			((x+1)%n,(y+1)%m),	 			
+			((x-1)%n,(y+1)%m),	 			
+			((x-1)%n,(y-1)%m),
+			((x-1)%n,y),
+			(x,(y+1)%m),
+			(x,(y-1)%m)
+]
+	if cord:
+		return vizinhosComuns		
+	else:
+		return sum([matriz[item[0]][item[1]] for item in vizinhosComuns])
+
+
+def simulaQuad(n,m,lista,t):
+	listOriginal = list(lista)	
+	for t in range(t):
+		matriz = geraMatriz(n,m,listOriginal)		
+		listaViva = []
+		for cel in listOriginal:
+			#I - Células Sobreviventes		
+			soma1 = calculaVizinhos(cel,matriz,n,m)			
+			if any([soma1==2,soma1==3]):
+				listaViva.append(cel)
+			VizinhosMortas = [[cord,calculaVizinhos(cord,matriz,n,m)] for cord in calculaVizinhos(cel,matriz,n,m,cord = True)]
+			listaViva.extend([morta[0] for morta in VizinhosMortas if morta[1]==3])			
+		listOriginal = list(set(listaViva))		
+	return listOriginal	
+ 		
+
+def desenhaQuad(n,m,lista,figura):
+	dim = (n,m)
+	mImprime = np.zeros(dim)	
+	for cel in lista:
+		mImprime[cel[0]][cel[1]] = r.randint(50,100)	
+	matriz = np.flipud(mImprime)
+
+	plt.matshow(matriz , vmin=0, vmax=100, cmap="PuRd")
+	plt.show()
+
 def leEntrada(nome):
 	with open(nome) as f:
 		content = [x.strip("\n") for x in f.readlines()]	
 	for x in range(1,len(content)):
-		content[x] = [int(t) for t in content[x].split(",")]
+		content[x] = tuple([int(t) for t in content[x].split(",")])		
 	return 0 if content[0]=="Q" else 1,content[1:]
-
-def simulaQuad(n,m,lista,t):
-	listOriginal = list(lista)
-	listaViva = []
-	for cel in lista:
-		x,y  = cel[0],cel[1] 
-		controle = 0 			 				
-		vizinhosComuns =[
-			[(x+1)%n,(y-1)%m],
-			[(x+1)%n,y],
-			[(x+1)%n,(y+1)%m],	 			
-			[(x-1)%n,(y+1)%m],	 			
-			[(x-1)%n,(y-1)%m],
-			[(x-1)%n,y],
-			[x,(y+1)%m],
-			[x,(y-1)%m]
-]
-		for vizinho in vizinhosComuns:
-			print("vizinho = ",vizinho)
-			if any((vizinho==celViva for celViva in listOriginal)):
-				listOriginal.remove(vizinho)
-				controle+=1
-				if controle>3:
-					break 			
-		if controle==2 or controle==3: 			
-			print("APPENDANDO",cel)
-			listaViva.append(cel)
-	return listaViva	
- 		
-
-def desenhaQuad(n,m,lista,figura):	
-	plt.matshow(geraMatriz(n,m,lista), vmin=0, vmax=1, cmap="PuRd")	
-	plt.show()
-
-
 	
 	
 if __name__ == '__main__':
