e273e2beffdfed4ca06de54fcf756b1843f0251a
juliano <juliano@JulianoDebian.home>
2016-06-02 20:33:01 -0300

    continua teste do hexagono

--- EP4/util.py
+++ EP4/util.py
@@ -7,15 +7,25 @@ import numpy as np
 fig, ax = plt.subplots(1)
 
 # positions
-pixel_x, pixel_y = np.indices((2, 2))
+pixel_x, pixel_y = np.indices((4, 4))
+pixel_x = np.array([0,1,2,3,4,5,0,1,2,3,4,5])
+pixel_y = np.array([0,0,0,0,0,0,1,1,1,1,1,1])
 
-pixel_color = np.random.random_sample(12).reshape(4, 3)
-print(len(pixel_color))
-dx = 5    # horizontal stride
-dy = 5    # vertical stride 
+
+pixel_color = np.array([[ 0.04502529 , 0.26985885 , 0.45650749] for t in range(11)])
+
 
 # set static radius
-poly_radius = 2.5
+raio = 7
+#numLinha
+linhas = 2
+colunas = 6
+poly_radius = raio
+
+dx = 2*poly_radius    # horizontal stride
+dy = 2*raio   # vertical stride 
+
+
 '''
 		dx = 5
 		dy = 5
@@ -28,13 +38,11 @@ patch_list = []
 
 # creat the patches
 controle = 0
-for c, x, y in zip(pixel_color, pixel_x.flat, pixel_y.flat):    
-	if controle%3==0:
-		dx = 5
-		dy = 5
-	else:
-		dx = 10
-		dy = 2.5
+ind = 0
+for c, x, y in zip(pixel_color, pixel_x.flat, pixel_y.flat):    	
+	if(ind%2==-0):
+		dx = 0
+
 	patch_list.append(
 			RegularPolygon(
 				xy=(x*dx, y*dy),
@@ -45,11 +53,12 @@ for c, x, y in zip(pixel_color, pixel_x.flat, pixel_y.flat):
 				edgecolor='k'  
 			)
 		)
+	controle+=1
 	
 
 
 pc = PatchCollection(patch_list, match_original=True)
 ax.add_collection(pc)
-
-ax.axis([-3, 80, -3, 80])
+ax.grid()
+ax.axis([-3, 100, -3, 100])
 plt.show()
