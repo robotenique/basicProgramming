a3a07e87e819b0caf05b3154531505fa0b367b44
juliano <juliano@JulianoDebian.home>
2016-06-01 21:32:03 -0300

    Terminado funções para quadrado

--- EP4/ep4.py
+++ EP4/ep4.py
@@ -2,22 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as r 
 def main():
-	n = m = 25
-	n = 17
-	t = 9
-	tipo, listaVivas = leEntrada("arquivo.txt")
-	
-	nova = simulaQuad(n,m,listaVivas,t)	
-
-	matriz = geraMatriz2(n,m,nova)
-	
-	for x in range(n):
-		for j in range(m):
-			print(matriz[x][j],end="")
-			print(" | ",end="")
-		print("\n")
-
-	desenhaQuad(n,m,nova,"nome")
+	n = m = 24
+	n = 24
+	t = 11
+	arquivo = "arquivo.txt"
+	tipo, listaVivas = leEntrada(arquivo)	
+	if tipo=="Q":
+		nova = simulaQuad(n,m,listaVivas,t)
+		desenhaQuad(n,m,nova,"fig1.png")
 
 def geraMatriz(n,m,listaVivas):	
 	dim = (n,m)
@@ -26,24 +18,8 @@ def geraMatriz(n,m,listaVivas):
 	for cel in vivas:
 		matriz[cel[0]][cel[1]] = 1
 	return matriz
-
-def geraMatriz2(n,m,listaVivas):	
-	dim = (n,m)
-	matriz = []
-	for i in range(n):
-		matriz.append([])
-		for j in range(m):
-			matriz[i]+=[" "]
-	vivas = list(listaVivas)
-	for cel in vivas:
-		matriz[cel[0]][cel[1]] = "X"
-	return matriz
-
-
-	#print(p)
-	#print(q)
 	
-def calculaVizinhos(cel,matriz,n,m,cord = False):
+def calculaVizinhosQ(cel,matriz,n,m,cord = False):
 	x,y = cel[0],cel[1]
 	vizinhosComuns =[
 			((x+1)%n,(y-1)%m),
@@ -68,10 +44,10 @@ def simulaQuad(n,m,lista,t):
 		listaViva = []
 		for cel in listOriginal:
 			#I - Células Sobreviventes		
-			soma1 = calculaVizinhos(cel,matriz,n,m)			
+			soma1 = calculaVizinhosQ(cel,matriz,n,m)			
 			if any([soma1==2,soma1==3]):
 				listaViva.append(cel)
-			VizinhosMortas = [[cord,calculaVizinhos(cord,matriz,n,m)] for cord in calculaVizinhos(cel,matriz,n,m,cord = True)]
+			VizinhosMortas = [[cord,calculaVizinhosQ(cord,matriz,n,m)] for cord in calculaVizinhosQ(cel,matriz,n,m,cord = True)]
 			listaViva.extend([morta[0] for morta in VizinhosMortas if morta[1]==3])			
 		listOriginal = list(set(listaViva))		
 	return listOriginal	
@@ -81,11 +57,15 @@ def desenhaQuad(n,m,lista,figura):
 	dim = (n,m)
 	mImprime = np.zeros(dim)	
 	for cel in lista:
-		mImprime[cel[0]][cel[1]] = r.randint(50,100)	
-	matriz = np.flipud(mImprime)
-
-	plt.matshow(matriz , vmin=0, vmax=100, cmap="PuRd")
-	plt.show()
+		mImprime[cel[0]][cel[1]] = r.randint(10,100)	
+	mImprime = np.flipud(mImprime)
+	fig = plt.figure(figsize=(4,3))
+	ax = fig.add_subplot(111)	
+	ax.matshow(mImprime, vmin=0, vmax=100, cmap="gist_ncar_r")
+	ax.set_yticks(())
+	ax.set_xticks(())
+	fig.savefig(figura, dpi = 300)	
+	
 
 def leEntrada(nome):
 	with open(nome) as f:
