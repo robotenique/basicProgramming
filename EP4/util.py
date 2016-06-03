7e4f7273993b8d7bbf91e5b39d94807b02369bdd
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-06-02 23:22:41 -0300

    ESSA porcaria de grade hexagonal funciona

--- EP4/util.py
+++ EP4/util.py
@@ -2,28 +2,26 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 from matplotlib.collections import PatchCollection
 import numpy as np
+import random as r
 
 # set up figure
 fig, ax = plt.subplots(1)
 
 # positions
 pixel_x, pixel_y = np.indices((4, 4))
-pixel_x = np.array([0,1,2,3,4,5,0,1,2,3,4,5])
-pixel_y = np.array([0,0,0,0,0,0,1,1,1,1,1,1])
+pixel_x = np.array([1,2,3,4,5,6,1,2,3,4,5,6])
+pixel_y = np.array([1,1,1,1,1,1,2,2,2,2,2,2])
 
 
-pixel_color = np.array([[ 0.04502529 , 0.26985885 , 0.45650749] for t in range(11)])
-
+pixel_color = cor = [[0.862745, 0.0784314, 0.235294] if x%2==0 else [0.541176, 0.168627 ,0.886275] for x in range(13)]
+#,[1,0.407421725309,0],[1,0,0],[ 1, 0, 0.85],[ 0.4,0,0.8520772],[0,0,1],[0.2,1,1]
 
 # set static radius
-raio = 7
+raio = 5
 #numLinha
 linhas = 2
 colunas = 6
 poly_radius = raio
-
-dx = 2*poly_radius    # horizontal stride
-dy = 2*raio   # vertical stride 
 
 
 '''
@@ -38,27 +36,51 @@ patch_list = []
 
 # creat the patches
 controle = 0
-ind = 0
-for c, x, y in zip(pixel_color, pixel_x.flat, pixel_y.flat):    	
-	if(ind%2==-0):
-		dx = 0
+indL = 1
+indC = 0
+for c, x, y in zip(pixel_color, pixel_x.flat, pixel_y.flat):	
+	#Se a coluna é par
+	apotema = np.sin(np.pi/3)*poly_radius
+	if (indC%linhas)%2==0: 
+		dx = 1.5*poly_radius
+		dy = apotema
+	#Se a coluna é ímpar
+	else:		
+		dx = 1.5*poly_radius
+		dy = 0
+	if(indL==2):
+		dy = 0
+		if(indC%linhas)%2!=0:
+			y = 1 + 2*apotema #1 + 2* apotema
+		else:
+			#CONSERTAR AQUI
+			y = 1 + 3*apotema
+
+	cord = (x*dx, y+dy)
 
 	patch_list.append(
 			RegularPolygon(
-				xy=(x*dx, y*dy),
+				xy=cord,
 				numVertices=6,
 				radius=poly_radius,
-				orientation=0.,
-				facecolor=c,
-				edgecolor='k'  
+				orientation=np.pi/2,
+				facecolor=c				
 			)
 		)
-	controle+=1
+
+	indC+=1
+	if indC == colunas:
+		indL+=1
+	print(indC)
+	
+
+	
 	
 
 
 pc = PatchCollection(patch_list, match_original=True)
 ax.add_collection(pc)
 ax.grid()
-ax.axis([-3, 100, -3, 100])
+ax.axis([-3, 50, -3, 50])
+ax.set_aspect('equal')
 plt.show()
