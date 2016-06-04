7a5b91af4a37d3c5e33443fd24a39f72c55e0c5c
juliano <juliano@JulianoDebian.home>
2016-06-04 14:23:07 -0300

    Adiciona calculo dos vizinhos hexagonais para celulas no meio da grade

--- EP4/ep4.py
+++ EP4/ep4.py
@@ -1,20 +1,31 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import RegularPolygon
+from matplotlib.collections import PatchCollection
 import numpy as np
 import random as r 
 def main():
 	#Entrada dos dados
-	m = 24
-	n = 24
-	t = 13
-	arquivo = "arquivo.txt"
+	m = 28
+	n = 17
+	t = 11
+	arquivo = "arquivo2.txt"
 
+	
+	#Gerador de Células Vivas Aleatórias (20 por vez. copiar do terminal p/ arquivo)
+	s = [(np.random.randint(0,n-1),np.random.randint(0,m-1)) for t in range(50)]
+	for t in s:
+		t =  str(t).replace(" ","")
+		print(t.strip("() "),sep="")
+	
 	tipo, listaVivas = leEntrada(arquivo)	
 	
 	if tipo==0:
 		nova = simulaQuad(n,m,listaVivas,t)
 		desenhaQuad(n,m,nova,"fig1.png")
+	else:
+		desenhaHex(n,m,listaVivas,"fig2.png")
 		
-def geraMatriz(n,m,listaVivas):	
+def geraMatrizQ(n,m,listaVivas):	
 	dim = (n,m)
 	matriz = np.zeros(dim)
 	vivas = list(listaVivas)
@@ -33,7 +44,7 @@ def calculaVizinhosQ(cel,matriz,n,m,cord = False):
 			((x-1)%n,y),
 			(x,(y+1)%m),
 			(x,(y-1)%m)
-]
+	]
 	if cord:
 		return vizinhosComuns		
 	else:
@@ -43,7 +54,7 @@ def calculaVizinhosQ(cel,matriz,n,m,cord = False):
 def simulaQuad(n,m,lista,t):
 	listOriginal = list(lista)	
 	for t in range(t):
-		matriz = geraMatriz(n,m,listOriginal)		
+		matriz = geraMatrizQ(n,m,listOriginal)		
 		listaViva = []
 		for cel in listOriginal:
 			#I - Células Sobreviventes		
@@ -65,7 +76,7 @@ def desenhaQuad(n,m,lista,figura):
 	#Matplotlib
 	fig = plt.figure(figsize=(4,3))
 	ax = fig.add_subplot(111)	
-	ax.matshow(mImprime, vmin=0, vmax=100, cmap="gist_ncar_r")	
+	ax.matshow(mImprime, vmin=-5, vmax=100, cmap="gist_ncar_r")	
 	ax.set_yticks(np.arange(0.5,m+(0.5)))
 	ax.set_xticks(np.arange(0.5,n+(0.5)))	
 	ax.set_xticklabels([])
@@ -73,14 +84,90 @@ def desenhaQuad(n,m,lista,figura):
 	ax.grid()	
 	fig.tight_layout()
 	fig.savefig(figura, dpi = 300)	
+
+def desenhaHex(n,m,lista,figura):
 	
+	#Variáveis Iniciais p/ Hexágono
+	vizinhos = []
+	patch_list = []
+	hexagonRatio = 5
+	indL,indC = 0 , 0	
+	apothem = np.sin(np.pi/3)*hexagonRatio
+	dx = 1.5 * hexagonRatio
+	#Matriz p/ grade hexagonal n x m
+	points_Y,points_X = (np.indices((n,m))+1)
+	
+	#Vizinhos TODO: OTIMIZAR AQUI!!
+	for x,y in lista:
+		if (y+1)%2==0:
+			vizinhos.extend([[x+1,y],[x,y+1],[x-1,y+1],[x-1,y],[x-1,y-1],[x,y-1]])
+		else:
+			vizinhos.extend([[x+1,y+1],[x+1,y],[x+1,y-1],[x,y-1],[x-1,y],[x,y+1]])
+
+	#Cores
+	cor = [[1, 1 ,1] for x in range(n*m)]
+	auxC = np.arange(0,n*m,m)	
+	for x,y in lista:		
+		cor[auxC[x]+y] = np.random.rand(3,1)
+	for x,y in vizinhos:		
+		cor[auxC[x]+y] = [0.5,0.5,1]		
+	
+	#Criação da lista de Patches com Hexágonos
+	for c, x, y in zip(cor, points_X.flat, points_Y.flat):	
+		#Coordenada coluna Ímpar
+		cordI = (x*dx,apothem * (2*indL+1))
+		#Coordenada coluna Par
+		cordP = (cordI[0],cordI[1]+apothem)	
+		#Tupla com ambas as coordenadas	
+		tCordenadas = (cordP,cordI)	
+		'''
+		-->Se tenho colunas pares, a ordem estabelecida é a normal.
+		-->Se tenho colunas ímpares, Preciso trocar a ordem a cada linha ,
+			(ou seja, quando a linha for par a ordem é trocada, e se for ímpar
+			 a ordem é mantida)
+		'''
+		if m%2==0:
+			cord =  tCordenadas[0] if indC%2==0 else tCordenadas[1]
+		else:
+			ehPar = (indL%2==0)
+			cord = tCordenadas[not(ehPar)] if indC%2==0 else tCordenadas[ehPar]	
+		#Incrementa o Índice de colunas e o índice de linhas
+		indC+=1
+		indL+=(indC%m == 0)
+		
+		#Cria um Hexágono com as características escolhidas
+		patch_list.append(
+				RegularPolygon(
+					xy=cord,
+					numVertices=6,
+					radius=hexagonRatio,
+					orientation=np.pi/2,
+					facecolor=c,
+					edgecolor = [0,0,0]			
+				)
+			)
+	#Configurando e salvando a figura
+	
+	fig = plt.figure(figsize=(4,3))
+	ax = fig.add_subplot(111)
+	pc = PatchCollection(patch_list, match_original=True)
+	ax.add_collection(pc)
+	ax.set_xlim(0,cordI[0]+dx)
+	ax.set_ylim(-hexagonRatio,cordI[1]+3*apothem)
+	ax.set_aspect('equal')
+	ax.set_xticks(())
+	ax.set_yticks(())
+	fig.tight_layout()
+	fig.savefig(figura, dpi = 300)
+
+
 
 def leEntrada(nome):
 	with open(nome) as f:
 		content = [x.strip("\n") for x in f.readlines()]	
 	for x in range(1,len(content)):
 		content[x] = tuple([int(t) for t in content[x].split(",")])		
-	return 0 if content[0]=="Q" else 1,content[1:]
+	return 0 if content[0]=="Q" else 1 , content[1:]
 	
 	
 if __name__ == '__main__':
--- EP4/util.py
+++ EP4/util.py
@@ -4,74 +4,64 @@ from matplotlib.collections import PatchCollection
 import numpy as np
 import random as r
 
-# set up figure
-fig, ax = plt.subplots(1)
-
-# positions
-pixel_x, pixel_y = np.indices((4, 4))
-pixel_x = np.array([1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6])
-pixel_y = np.array([1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6])
 
 
-pixel_color = cor = [[0.862745, 0.0784314, 0.235294] if x%2==0 else [0.541176, 0.168627 ,0.886275] for x in range(36)]
-#,[1,0.407421725309,0],[1,0,0],[ 1, 0, 0.85],[ 0.4,0,0.8520772],[0,0,1],[0.2,1,1]
-
-# set static radius
-raio = 5
-#numLinha
-linhas = 6
-colunas = 6
-poly_radius = raio
-
-
-'''
-		dx = 5
-		dy = 5
-	else:
-		dx = 10
-		dy = 2.5
-'''
-# list to hold patches
+# Variaveis iniciais
+n = 5
+m = 10
 patch_list = []
-
-# creat the patches
-controle = 0
+hexagonRatio = 5
 indL = 0
 indC = 0
-for c, x, y in zip(pixel_color, pixel_x.flat, pixel_y.flat):	
-	print(c)
-	apotema = np.sin(np.pi/3)*poly_radius
-	yI = apotema * (2*indL+1)
-	dx = 1.5 * poly_radius
-	if indC%2==0:
-		yP = yI + apotema
-		cord = (x*dx,yP)
+apothem = np.sin(np.pi/3)*hexagonRatio
+dx = 1.5 * hexagonRatio
+
+
+points_Y,points_X = (np.indices((n,m))+1)
+
+cor = [np.random.rand(3,1) if x%2==0 else [0, 1 ,0.3] for x in range(1100)]
+
+
+
+
+for c, x, y in zip(cor, points_X.flat, points_Y.flat):	
+	cordI = (x*dx,apothem * (2*indL+1))
+	cordP = (cordI[0],cordI[1]+apothem)	
+	tCordenadas = (cordP,cordI)	
+	if m%2==0:
+		cord =  tCordenadas[0] if indC%2==0 else tCordenadas[1]
 	else:
-		cord = (x*dx,yI)
+		ehPar = (indL%2==0)
+		cord = tCordenadas[not(ehPar)] if indC%2==0 else tCordenadas[ehPar]	
+	
+	indC+=1
+	indL+=(indC%m == 0)
+	
+	#Cria um Hexágono com as características escolhidas
 	patch_list.append(
 			RegularPolygon(
 				xy=cord,
 				numVertices=6,
-				radius=poly_radius,
+				radius=hexagonRatio,
 				orientation=np.pi/2,
-				facecolor=c				
+				facecolor=c,
+				edgecolor = [0,0,0]			
 			)
 		)
 
-	indC+=1
-	if indC%colunas == 0:
-		print("entrou")
-		indL+=1
-	print(indL)
-	
 
-	
-	
+#Coordenadas do primeiro e último hexágono da grid
 
 
+# set up figure
+fig, ax = plt.subplots(1)
+fig = plt.figure(figsize=(4,3))
+ax = fig.add_subplot(111)
 pc = PatchCollection(patch_list, match_original=True)
 ax.add_collection(pc)
-ax.grid()
-ax.axis([-3, 50, -3, 50])
+ax.set_xlim(0,cordI[0]+dx)
+ax.set_ylim(-hexagonRatio,cordI[1]+3*apothem)
 ax.set_aspect('equal')
+ax.set_xticks(())
+ax.set_yticks(())
 plt.show()
