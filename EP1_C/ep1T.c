2cd95ab2fe77ec4d7b5cecd7f1f5561e0e0b4c97
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-08-04 18:29:37 -0300

    Add a diferent method to return if a number is a log in the base 2

--- EP1_C/ep1T.c
+++ EP1_C/ep1T.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
+#include <math.h> 
 
 long long int colz(long long int n);
 long long int recColz(long long int n);
+int calcLog2(long long int v);
 
+/* One liner: Check if a number is a power of 2: (n != 0) && ((n & (~n + 1)) == n)*/
 
-int cont = 0;
 int main()
 {	
 
 	/* Main process */
-	/*
-	gcc -Wall -Wextra -std=c99 -o ep1 ep1T.c -Ofast -fwhole-program
+	/* 
+	gcc -Wall -Wextra -std=c99 -O4 -o ep1 ep1T.c -Ofast -fwhole-program
+
 
 	int val;
 	int vezes;
 	val = 23;
+
+	TESTE x = 55555555 iterações:
+	 --> COM LOG (LinuxLow): 
+	 	1m6.878s
+	 	1m0.982s
+	 --> SEM LOG (LinuxLow):
+		0m39.800s
+
+
+
+
+
 	*/	
 	
 	
-	for (long long int i = 1; i < 10000000; i++)	
+
+
+
+	for (long long int i = 1; i < 55555555; i++)	
 		{
 		//printf("i = %llu ,  %llu\n",i,colz(i));
-		recColz(i);
+		recColz(i);		
 		//colz(i);
 		}
-	printf("%d\n",cont);
-
-	
 
 	return 0;
 }
@@ -52,8 +67,10 @@ long long int colz(long long int n)
 }
 long long int recColz(long long int n)
 {	
-	if ((n != 0) && ((n & (~n + 1)) == n))
-		cont += 1;
+
+	if ((n != 0) && ((n & (~n + 1)) == n))		
+		return (int)log2(n);
+		
 	if(n==1)
 		return 0;
     if(n%2==0)
@@ -61,4 +78,17 @@ long long int recColz(long long int n)
     else
         return (1+recColz(3*n+1));	
 
+}
+
+
+int calcLog2(long long int v)
+{
+static const long long int b[] = {0xAAAAAAAA, 0xCCCCCCCC, 0xF0F0F0F0, 
+                                 0xFF00FF00, 0xFFFF0000};
+register long long int r = (v & b[0]) != 0;
+for (int i = 4; i > 0; i--) // unroll for speed...
+{
+  r |= ((v & b[i]) != 0) << i;
+}
+return r;
 }
--- EP1_C/ep1pTest.py
+++ EP1_C/ep1pTest.py
@@ -1,16 +1,39 @@
 import time
+import sys
+
+
+
+from math import log2
 time1 = time.time()
 
 def main():	
 	mA = 0
 	aN = 0
 	som = 0
-	for X in range(1,1000000):		
-		som+=colz(X)	
+
+	#List of 2^n numbers up until 2^32:
+	
+	listPow = []
+	for x in range(1,60):
+		listPow.append(2**x)	
+
+	
+	orig_stdout = sys.stdout
+	f = open('out.txt', 'w')
+	sys.stdout = f
+	for x in listPow:
+		print(x)
+	sys.stdout = orig_stdout
+	f.close()
+	
 	print("--- %s seconds ---" % (time.time() - time1))
-	print('Soma = ',som)
-	time2 = time.time()
+	
+
+
+
 	'''
+	time2 = time.time()
+	
 	for X in range(1,1000000):		
 		calculateSteps(X)
 	print("--- %s seconds ---" % (time.time() - time2))
@@ -21,13 +44,18 @@ def colz(n):
 	i = 0
 		
 	while n!=1:		
-		while n%2!=0:
+		while n%2!=0:			
 			n = (3*n + 1)/2
 			i+=2
-		while n%2==0:
+		while n%2==0:			
 			n /= 2
 			i += 1
 	return i
+
+
+
+def is2(num):
+	return ((num & (num - 1)) == 0) and num != 0
 
 #Default function
 def calculateSteps(n):
