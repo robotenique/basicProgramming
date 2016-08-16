d94d53e3674e4e2960245559d47070188f7742bb
Juliano <julianogarcia_1997@hotmail.com>
2016-08-16 13:24:27 -0300

    Add final adjustments into the program, leaving only the standarization of the code

--- EP1_C/ep1T.c
+++ EP1_C/ep1T.c
@@ -1,7 +1,8 @@
 #include <stdio.h> /* printf, scanf, NULL */
 #include <stdlib.h> /* malloc, free, rand */
 
-/* Mudar nome das variaveis */
+/* Mudar nome das variaveis 2147483647 1000000 */
+
 typedef long int intL;
 static unsigned short int *numbersCalc;
 
@@ -9,8 +10,9 @@ unsigned short int calcCollatz(int n)
 {
 	unsigned int auxN = n;
 	unsigned short int steps = 1;
+	
 	while(1)
-	{
+	{		
 		if(auxN%2!=0)
 		{
 			auxN = 3*auxN + 1;
@@ -43,14 +45,25 @@ int main()
 	/* Versão 1 - Os índices vão de {0,1,2,...,end} endereços */
 	scanf("%u %u",&begin,&end);
 	numbersCalc = malloc(end * sizeof(numbersCalc[0]));	
-	if (numbersCalc == NULL)	
+	if (numbersCalc == NULL) {
 		printf("Alert: Failure in memory allocation, the processing time can increase drastically.\n");	
-	numbersCalc[1] = 1;
-	for (i = 2; i <= end; i++){		
-		numbersCalc[i] = calcCollatz(i);		
-		if (i>=begin)
-			printf("colz(%u) = %hu\n", i,numbersCalc[i]-1 );
+		return 1;
 	}
+	numbersCalc[1] = 0;	
+	if (begin==1)
+	{
+		printf("0\n");
+		begin ++;
+	}
+	for (i = begin; i <= end; i++){		
+		numbersCalc[i] = calcCollatz(i);		
+		
+		/*if (i>=begin)
+			printf("colz(%u) = %hu\n", i,numbersCalc[i] );
+		*/
+	}
+	free(numbersCalc)
+	
 
 	return 0;
 }
