8a33ad9de2d0688b0b092e0d58d934482953d4c0
Juliano <julianogarcia_1997@hotmail.com>
2016-08-24 20:50:53 -0300

    Alter the function justifyParagraph(), which is now almost working. The bugs are: It doesn't add the final world of the paragraph, and don't calculate  a new line if the length of the word is bigger than the cols number

--- MAC216/previaEP.py
+++ MAC216/previaEP.py
@@ -7,12 +7,10 @@ def main():
 	text	 = readEntry(fileName)	
 	text     = text.split('\n\n')
 	text	 = [re.split('\n| ',t) for t in text]
+	#testWhile()	
 	
-	justifyParagraph(text[0],cols)
-	print(text[0])
-
 	#for paragraph in text:
-	#	justifyParagraph(paragraph,cols)
+	justifyParagraph(text[0],cols)
 	
 	
 '''
@@ -30,29 +28,36 @@ def main():
 
 '''
 
+
+
+
+
+
+
+
+
 #TODO: IMPLEMENT CASE WHEN THE SINGLE WORD IS LARGER THAN COLUMNS
 def justifyParagraph(paragraph,cols):
-	bufferW = ""	
-	count   = 0
-	i       = 0
-	j       = 0
-	if(len(paragraph)%2!=0):
-		paragraph.append("")	
-	#While the actual numbers of characters + length of next word + number of spaces is lower|equal to cols
-	while(j < len(paragraph)):		
-		count = 0
-		i     = j
-		while (i < len(paragraph) - 1) and (count + len(paragraph[i+1]) + ceil(i+2)/2 <= cols):
-			print("BufferW <- p[",i,"] + p[",i+1,"]")
-			bufferW += paragraph[i] + "_" + paragraph[i+1]+"_"
-			count   += len(paragraph[i])
-			i += 2
-		bufferW += "\n"		
+	print(len(paragraph))
+	bufferW = ""
+	i = 0
+	while i <= len(paragraph) -1 :
+		sL = ""		
+		sumT = 0
 		j = i
-		j += 1	
-		print("I NOW = ",i)
-		print(bufferW)
+		if len(paragraph[i])
+		
+		while i + 1 < len(paragraph) and sumT + len(paragraph[i+1]) + ceil(i-j+2)/2 <= cols:			
+			sumT+= len(paragraph[i+1]) + ceil(i-j+2)/2
+			i += 1
+		print("I agora = ",i)			
 
+		for x in range(j,i):
+			sL += paragraph[x]+"_"			
+		sL = sL[:len(sL)-1]
+		sL += "\n"
+		bufferW += sL
+	print(bufferW)
 
 
 
