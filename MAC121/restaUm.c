8145332a33b6371bf60fc699a4200935021fc030
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-09-22 22:45:51 -0300

    Refactor and fix a lot of bugs in the main recursive function. Add new pretty print functions to debug the program

--- MAC121/restaUm.c
+++ MAC121/restaUm.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dataStructs.h"
+#include <unistd.h>
 
 
 #define KMAG  "\x1B[35m"
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
+#define KYEL  "\x1B[33m"
+#define KRED  "\x1B[31m"
+#define KBLU  "\x1B[34m"
+
+
 
 minINT ** criaTabuleiro(int m, int n);
-int * getDadosTabuleiro (minINT **tab, int m, int n, posArray *posB, posArray *posP);
-bool resolveRestaUm (minINT ** tab, int m, int n,int pecas,int buracos,posArray *posB,posArray *posP);
-bool fazMovimento (minINT **tab , int m, int n, int pecas, posArray *posP, int j, minINT mov);
-void desfazMovimento (minINT **tab, int m, int n, int pecas, posArray *posP, int j, minINT mov);
-bool estaResolvido (posArray *posB, minINT **tab, int pecas, int buracos);
-minINT podeMover(minINT **tab, int m, int n, pos p, minINT mov);
+int * getDadosTabuleiro (minINT **tab, int m, int n, posArray *posB);
+bool resolveRestaUm (minINT ** tab, int m, int n,int buracos,posArray *posB);
+bool fazMovimento (minINT **tab , int m, int n, int j, minINT mov);
+void desfazMovimento (minINT **tab, int m, int n,int j, minINT mov);
+bool estaResolvido (posArray *posB, minINT **tab,int buracos);
+minINT podeMover(minINT **tab, int m, int n, int j, minINT mov);
 bool haPecas_buracos(posArray *posB,minINT **tab);
+/*-------------DEBUG-------------*/
 void debugPosicoes (posArray *pArr);
 void imprimeTab (minINT **tab,int m, int n);
+void lixou (minINT **tab, int l, int c, minINT mov, int m, int n);
+
+
+int pecas = 0;
 
 int main (int argc, char const *argv[]) {
     /*Declaração de variáveis */
-    int m,n,i,j,pecas,buracos,aux = 0;
+    int m,n,i,j,buracos,aux = 0;
     int *dados;
     minINT ** tab;
-    posArray *posB,*posP;
+    posArray *posB;
 
     /*Leitura de dados */
     scanf("%d%d",&m,&n);
@@ -36,13 +47,12 @@ int main (int argc, char const *argv[]) {
         }
     /*Inicialização de dados */
     posB = criaPosArray(1);
-    posP = criaPosArray(2);
-    dados = getDadosTabuleiro(tab,m,n,posB,posP);
+    dados = getDadosTabuleiro(tab,m,n,posB);
     pecas = dados[0];
     buracos = dados[1];
     free(dados);
 
-    if(resolveRestaUm (tab,m,n,pecas,buracos,posB,posP))
+    if(resolveRestaUm (tab,m,n,buracos,posB))
         printf("TRUE\n");
     else{
         printf("NAOOOOOOOO\n");
@@ -66,17 +76,21 @@ minINT ** criaTabuleiro (int m, int n) {
     return tab;
 }
 
-bool resolveRestaUm (minINT ** tab, int m, int n, int pecas,int buracos,posArray *posB,posArray *posP) {
+bool resolveRestaUm (minINT ** tab, int m, int n,int buracos,posArray *posB) {
     /* CUIDADO COM O FATO DOS posArray serem passados por referencia!! */
     int j;
     minINT mov;
-    if(estaResolvido(posB,tab,pecas,buracos))   return true;
-    for (j = 0; j < posP->i; j++)
-        for (mov = 1; mov < 5 ; mov++)
-            if (fazMovimento(tab,m,n,pecas,posP,j,mov)) {
-                if(resolveRestaUm(tab,m,n,pecas,buracos,posB,posP)) return true;
-                desfazMovimento(tab,m,n,pecas,posP,j,mov);
-            }
+    /*sleep(1);*/
+    if(estaResolvido(posB,tab,buracos)){imprimeTab(tab,m,n); return true;}
+    else if (pecas <= n) return false;
+    for (j = 0; j < m*n; j++){
+    /*printf("[%d][%d]\n",j,n); sleep(1);*/
+        if(tab[j/n][j%n]==1){
+            for (mov = 1; mov < 5 ; mov++)
+                if (fazMovimento(tab,m,n,j,mov)) {
+                    if(resolveRestaUm(tab,m,n,buracos,posB)) return true;
+                    desfazMovimento(tab,m,n,j,mov);
+                }}}
     return false ;
 }
 /*
@@ -91,7 +105,7 @@ bool resolveRestaUm (minINT ** tab, int m, int n, int pecas,int buracos,posArray
  *   return: Vetor de três posições: [0] = nº de peças e [1] = número de buracos
  */
 
-int * getDadosTabuleiro (minINT **tab, int m, int n, posArray *posB, posArray *posP) {
+int * getDadosTabuleiro (minINT **tab, int m, int n, posArray *posB) {
     int i,j;
     pos nPos;
     int pecas = 0,buracos = 0;
@@ -103,27 +117,24 @@ int * getDadosTabuleiro (minINT **tab, int m, int n, posArray *posB, posArray *p
     }
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
-            if (tab[i][j]==1) {
+            if (tab[i][j]==1)
                 pecas++;
-                nPos.x = i;
-                nPos.y = j;
-                adicionaPos(posP,nPos);
-            }
             else if (tab[i][j]==-1) {
                 buracos++;
                 nPos.x = i;
                 nPos.y = j;
                 adicionaPos(posB,nPos);
-
             }
     dados[0] = pecas;
     dados[1] = buracos;
     return dados;
 }
 
-bool estaResolvido (posArray *posB, minINT **tab, int pecas, int buracos) {
-    if (pecas == buracos)
+bool estaResolvido (posArray *posB, minINT **tab, int buracos) {
+    if (pecas == buracos){
+        printf("iS = %d\n",pecas );
         return haPecas_buracos(posB,tab);
+    }
     return false;
 }
 
@@ -135,7 +146,9 @@ bool haPecas_buracos (posArray *posB, minINT **tab ) {
     return true;
 }
 
-minINT podeMover(minINT **tab, int m, int n, pos p, minINT mov) {
+minINT podeMover(minINT **tab, int m, int n, int j, minINT mov) {
+    int l = j/n, c = j%n;
+    if(l > 6)printf("(pM) L = %d ",l );
     /*
      *    Movimentos possíves
      *           (2)
@@ -155,73 +168,95 @@ minINT podeMover(minINT **tab, int m, int n, pos p, minINT mov) {
      */
     switch (mov) {
         case 1:
-            if(p.y - 2 < 0 || tab[p.x][p.y-1]!=1 || tab[p.x][p.y-2] != -1) return 0;
+            if(c - 2 < 0 || tab[l][c-1]!=1 || tab[l][c-2] != -1) return 0;
             break;
         case 2:
-            if(p.x - 2 < 0 || tab[p.x-1][p.y]!=1 || tab[p.x-2][p.y] != -1) return 0;
+            if(l - 2 < 0 || tab[l-1][c]!=1 || tab[l-2][c] != -1) return 0;
             break;
         case 3:
-            if(p.y + 2 > n-1 || tab[p.x][p.y+1]!=1 || tab[p.x][p.y+2] != -1) return 0;
+            if(c + 2 > n-1 || tab[l][c+1]!=1 || tab[l][c+2] != -1) return 0;
             break;
         case 4:
-            if(p.x + 2 > m-1 || tab[p.x+1][p.y]!=1 || tab[p.x+2][p.y] != -1) return 0;
+            if(l + 2 > m-1 || tab[l+1][c]!=1 || tab[l+2][c] != -1) return 0;
             break;
     }
     return mov;
 }
 
-bool fazMovimento (minINT **tab , int m, int n, int pecas, posArray *posP, int j,minINT mov) {
-    int movN;
-    pos p = posP->p[j];
-    if((movN = podeMover(tab,m,n,p,mov)))
+bool fazMovimento (minINT **tab , int m, int n, int j,minINT mov) {
+    minINT movN;
+    int l=0,c=0;
+    if((movN = podeMover(tab,m,n,j,mov))) {
+        l = j/n;
+        c = j%n;
         switch (movN) {
             case 1:
-                tab[p.x][p.y-2] = 1;
-                tab[p.x][p.y-1] = -1;
-                tab[p.x][p.y] = -1;
+                tab[l][c-2] = 1;
+                tab[l][c-1] = -1;
+                tab[l][c] = -1;
                 pecas--;
-                posP->p[j].y = p.y - 2;
                 return true;
             case 2:
-                tab[p.x-2][p.y] = 1;
-                tab[p.x-1][p.y] = -1;
-                tab[p.x][p.y] = -1;
+                tab[l-2][c] = 1;
+                tab[l-1][c] = -1;
+                tab[l][c] = -1;
                 pecas--;
-                posP->p[j].x = p.x - 2;
                 return true;
             case 3:
-                tab[p.x][p.y+2] = 1;
-                tab[p.x][p.y+1] = -1;
-                tab[p.x][p.y] = -1;
+                tab[l][c+2] = 1;
+                tab[l][c+1] = -1;
+                tab[l][c] = -1;
                 pecas--;
-                posP->p[j].y = p.y + 2;
                 return true;
             case 4:
-                tab[p.x+2][p.y] = 1;
-                tab[p.x+1][p.y] = -1;
-                tab[p.x][p.y] = -1;
+                tab[l+2][c] = 1;
+                tab[l+1][c] = -1;
+                tab[l][c] = -1;
                 pecas--;
-                posP->p[j].x = p.x + 2;
                 return true;
         }
+
+    }
     return false;
 }
 
-void desfazMovimento (minINT **tab, int m, int n, int pecas, posArray *posP, int j, minINT mov) {
-    int movN;
-    pos p = posP->p[j];
+void desfazMovimento (minINT **tab, int m, int n,int j, minINT mov) {
+    int l = j/n, c = j%n;
+    printf("%s                           ANTES\n",KYEL );
+    imprimeTab(tab,m,n);
     switch (mov) {
         case 1:
-            tab[p.x][p.y] = -1;
-            tab[p.x][p.y+1] =1;
-            tab[p.x][p.y+2] =1;
+            tab[l][c] = 1;
+            tab[l][c-1] = 1;
+            tab[l][c-2] = -1;
             pecas++;
-            posP->p[j].y = p.y + 2;
-
+            lixou(tab,l,c,mov,m,n);
+            break;
         case 2:
+            tab[l-2][c] = -1;
+            tab[l-1][c] = 1;
+            tab[l][c] = 1;
+            pecas++;
+            lixou(tab,l,c,mov,m,n);
+            break;
         case 3:
+            tab[l][c] = -1;
+            tab[l][c-1] =1;
+            tab[l][c-2] =1;
+            pecas++;
+            lixou(tab,l,c,mov,m,n);
+            break;
         case 4:
+            tab[l+2][c] = -1;
+            tab[l+1][c] = 1;
+            tab[l][c] = 1;
+            pecas++;
+            lixou(tab,l,c,mov,m,n);
+            break;
     }
+    printf("%s                           DEPOIS\n",KBLU );
+    printf("                    |%s tab[%d][%d], mov = %d|\n",KBLU,l,c,mov);
+    imprimeTab(tab,m,n);
 }
 
 void debugPosicoes (posArray *pArr) {
@@ -231,15 +266,27 @@ void debugPosicoes (posArray *pArr) {
 
 void imprimeTab (minINT **tab,int m, int n) {
     int i,j;
+    printf("%s ============================================================\n",KRED);
+
     for (i = 0; i < m; i++) {
+        printf("%s |                   ",KRED);
         for (j = 0; j < n; j++){
             if (tab[i][j]==0)
-                printf("%s%3d",KWHT,tab[i][j]);
+                printf("%s-  ",KWHT);
             else if (tab[i][j]==1)
-                printf("%s%3d",KCYN,tab[i][j]);
+                printf("%sO  ",KCYN);
             else
-                printf("%s%3d",KMAG,tab[i][j]);
+                printf("%sx  ",KMAG);
         }
-        printf("\n");
+        printf("%s                  |\n",KRED);
+    }
+    printf("%s ============================================================\n",KRED);
+}
+
+void lixou (minINT **tab, int l, int c, minINT mov, int m, int n) {
+    if (tab[6][0]!=0 || tab[6][1]!=0 || tab[5][0]!=0 || tab[5][1]!=0) {
+        printf("%s>>>>>>>>>> LIXOU!! DbLog = {tab[%d][%d], mov = %d} <<<<<<<<<<<<\n",KBLU,l,c,mov);
+        imprimeTab(tab,m,n);
+        exit(-1);
     }
 }
