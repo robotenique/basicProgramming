701911beeec1591c5c01d5aebba8034571089400
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-09-28 22:58:21 -0300

    Add python program with a GUI which calculates and show a plot of relative frequency

--- RandomAcessMemory/tk_interface.py
+++ RandomAcessMemory/tk_interface.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-from random import randint
 
 import sys
 if sys.version_info[0] < 3:
@@ -16,10 +15,49 @@ else:
 
 
 # plotting function: clear current, plot & redraw
-def plot(x, y):
+def plot(lim):
+    # Clear current plot
     plt.clf()
-    plt.plot(x,y)
-    # just plt.draw() won't do it here, strangely
+    lim = int(lim.get())
+    fRelMatrix = [[] for y in range(6)]
+    x_axis = [x for x in range(1, lim+1)]
+    uniform_list = np.random.randint(1, size=lim, high=7)
+    cor = [[1, 0, 0.85], [0.4, 0, 0.8520772], [0, 0, 1], [0.2, 1, 1],
+    [0.2, 1, 0], [0.9, 0.973553065884, 0.09], [1, 0.407421725309, 0], [1, 0, 0]]
+    z = np.arange(6)+2
+    data_freq = np.array([0, 0, 0, 0, 0, 0])
+    i = 0
+
+    for num in uniform_list:
+        i += 1
+        data_freq[num-1] += 1
+        for j in range(6):
+            fRelMatrix[j].append(data_freq[j]/i)
+    for j in range(6):
+        if(i < 10000):
+            plt.plot(x_axis, fRelMatrix[j], linewidth=3, color=cor[j])
+        plt.scatter(x_axis, fRelMatrix[j], marker='+', c=cor[j], zorder=z[j])
+
+    # Axis configuration
+    plt.ylim(0, 1)
+    plt.xlim(0, lim)
+    plt.xlabel('Nº jogadas', fontsize=18)
+    plt.ylabel('Freq. Relativa', fontsize=18)
+    ax = plt.gca()
+    plt.tight_layout()
+
+    ax.spines['bottom'].set_color('k')
+    ax.spines['left'].set_color('k')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.set_axis_bgcolor('k')
+    ax.xaxis.label.set_color('k')
+    ax.yaxis.label.set_color('k')
+    ax.tick_params(axis='x', colors='k')
+    ax.tick_params(axis='x', colors='k')
+
     plt.gcf().canvas.draw()
 
 
@@ -27,36 +65,40 @@ def onCalc(nEntry):
         errmsg = "Digite um número inteiro válido!"
         try:
             n = int(nEntry.get())
-            x = np.arange(0.0,3.0,0.01)
-            y = np.sin(2*np.pi*x+n)
-            plot(x, y)
-
+            plot(nEntry)
         except ValueError:
             messagebox.showerror("Entrada inválida", errmsg)
             return
-        print(n)
 
-# GUI
-root = Tk()
-root.title('Gráfico de frequência')
-root.resizable(width=FALSE, height=FALSE)
 
-nLabel = ttk.Label(root, text="N =")
-nEntry = ttk.Entry(root, width=12)
-nEntry.insert(0, "6")
-draw_button = ttk.Button(root, text="Plot!", width=15, command = lambda:onCalc(nEntry))
+def createInterface():
+    # GUI
+    root = Tk()
+    root.title('Gráfico de frequência')
+    root.resizable(0, 0)
+    nLabel = ttk.Label(root, text="N =")
+    nEntry = ttk.Entry(root, width=12)
+    nEntry.insert(0, "60")
+    draw_button = ttk.Button(root, text="Plot!", width=15, command=lambda: onCalc(nEntry))
+    # Layout
+    nLabel.grid(row=0,column=0, pady=5, sticky=N)
+    nEntry.grid(row=0,column=1, pady=5, padx=10, sticky=N)
+    draw_button.grid(row=0, column=0, padx=5, sticky="we")
 
-#Layout
-nLabel.grid(row=0,column=0, pady=5, sticky=N)
-nEntry.grid(row=0,column=1, pady=5, padx=10, sticky=N)
-draw_button.grid(row=0, column=0, padx=5, sticky="we")
+    # init figure
+    fig = plt.figure()
+    fig.set_size_inches(18.5, 10.5)
 
-# init figure
-fig = plt.figure()
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    toolbar = NavigationToolbar2TkAgg(canvas, root)
+    canvas.get_tk_widget().grid(row=0, column=2)
+    toolbar.grid(row=1, column=2)
 
-canvas = FigureCanvasTkAgg(fig, master=root)
-toolbar = NavigationToolbar2TkAgg(canvas, root)
-canvas.get_tk_widget().grid(row=0,column=2)
-toolbar.grid(row=1,column=2)
+    root.mainloop()
 
-root.mainloop()
+
+def main():
+    createInterface()
+
+if __name__ == '__main__':
+    main()
