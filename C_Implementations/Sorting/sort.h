47a751cf27a5c1070e19bf2b31074cda337fff96
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-10-10 09:56:47 -0300

    Add sorting algorithm prototype files

---
+++ C_Implementations/Sorting/sort.h
@@ -0,0 +1,12 @@
+#ifndef _SORT_H_
+#define  _SORT_H_
+
+int linearSearch();
+int bubbleSort();
+int insertionSort();
+int selectionSort();
+int mergeSort();
+int quickSort();
+int heapSort();
+
+#endif
