a49f05b3d4e2d995a51aa154cc414b4768096bd3
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-10-13 13:49:19 -0300

    Add new files for ep3 and remove old binary files from ep2

---
+++ MAC121/EP3/arrayOp.c
@@ -0,0 +1,44 @@
+#include "arrayOp.h"
+
+void swap (int v[], int x, int y);
+
+void toDown (int v[], int n, int i) {
+    int x = v[i];
+    int parent = i;
+    int son = 2*i+1;
+    while (son < n) {
+        if (son + 1 < n && v[son + 1] > v[son])
+            son = son + 1;
+        if (x > v[son])
+            break;
+        v[parent] = v[son];
+        parent = son;
+        son = 2*son + 1;
+    }
+    v[parent] = x;
+}
+
+void heapfy (int v[], int n) {
+    int i;
+    for (i = (n-2)/2; i >=0; toDown(v, n, i), i--);
+}
+
+void heapSort (int v[], int n) {
+    int i;
+    heapfy(v, n);
+    for (i = n-1; i > 0; i--){
+        swap(v, 0, i);
+        toDown(v, i, 0);
+    }
+}
+
+void swap (int v[], int x, int y) {
+    int aux = v[x];
+    v[x] = v[y];
+    v[y] = aux;
+}
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
---
+++ MAC121/EP3/arrayOp.h
@@ -0,0 +1,8 @@
+#ifndef _ARRAYOP_H_
+#define _ARRAYOP_H_
+
+/* Implementa um heap sort */
+void heapSort (int v[], int n);
+void toDown (int v[], int n, int i);
+void heapfy (int v[], int n);
+#endif
