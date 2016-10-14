86b5190b4390d4bad56d755ac38af03781bd5229
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-10-14 13:21:35 -0300

    Add simplified version of sorting for Even numbers

--- MAC121/EP3/tresReversao.c
+++ MAC121/EP3/tresReversao.c
@@ -11,10 +11,13 @@
 
 int* createArray(int n);
 bool sortArrayCustom (int **v, int n);
-void bubbleThreeSort (int *v, int *s, int n);
+void bThreeSortEven (int **v, int **s, int n);
+void bThreeSortOdd (int **v, int **s, int n);
 int mod(int a, int b);
+void swapPos (int **v, int x, int y);
+
 int main(int argc, char const *argv[]) {
-    int n, k;
+    int n, i;
     int **v;
     /* Leitura */
     scanf("%d",&n);
@@ -22,15 +25,17 @@ int main(int argc, char const *argv[]) {
     if(v == NULL) exit(-1);
     v[0] = malloc(n*sizeof(int));
     v[1] = malloc(n*sizeof(int));
-    for (k = 0; k < 2; checkArray(v[k]), k++);
-    for (k = 0; k < n; k++) {
-        scanf ("%d", &v[0][k]);
-        v[1][k] = k%2;
+    for (i = 0; i < 2; checkArray(v[i]), i++);
+    for (i = 0; i < n; i++) {
+        scanf ("%d", &v[0][i]);
+        v[1][i] = i;
     }
+    /*************************************/
     printf("----Original----\n");
     printArray(v[0],n);
     printArray(v[1],n);
-    if(!sortArrayCustom (v, n))
+    /*************************************/
+    if(n < 3 || !sortArrayCustom (v, n))
         printf("Nao e possivel\n");
 
 
@@ -44,7 +49,7 @@ bool sortArrayCustom (int **v, int n) {
     sortV[0] = malloc(n*sizeof(int));
     sortV[1] = malloc(n*sizeof(int));
     if(sortV == NULL) exit(-1);
-    for(k = 0; k < n; sortV[0][k] = v[0][k], sortV[1][k] = k % 2, k++);
+    for(k = 0; k < n; sortV[0][k] = v[0][k], sortV[1][k] = k, k++);
     for (k = 0; k < 2; checkArray(v[k]), k++);
     heapSort(sortV, n);
     if(!(n % 2)) {
@@ -52,22 +57,51 @@ bool sortArrayCustom (int **v, int n) {
          * para posição ímpar se for movendo de 3 em 3, e vice-versa
          * para os pares.
          */
+        /*************************************/
         printf("----Calculado----\n");
         printArray(sortV[0],n);
         printArray(sortV[1],n);
-        for (k = 0; k < n && (sortV[1][k] == v[1][k]); k++);
+        /*************************************/
+        for (k = 0; k < n && (sortV[1][k]%2 == v[1][k]%2); k++);
         if (k != n)
             return false;
-        bubbleThreeSort(v[0], sortV[0], n);
-
+        bThreeSortEven(v, sortV, n);
+    }
+    else {
+        bThreeSortOdd(v, sortV, n);
     }
     return true;
 }
 
-void bubbleThreeSort (int *v, int *s, int n) {
+void bThreeSortEven (int **v, int **s, int n) {
+    int i, k = 0;
+    /* Sorting dos números nas posições pares */
+    for (i = n - 2; i >= 0; i -= 2) {
+        k = s[1][i];
+        while(v[0][i] != s[0][i]) {
+            printf("%d\n",k);
+            swapPos(v, k, mod(k + 2, n));
+            k = mod(k + 2, n);
+        }
+    }
+    printArray(v[0],n);
+    /* Sorting dos números nas posições ímpares */
+    for (i = n - 1; i >=1; i -= 2) {
+        k = s[1][i];
+        while (v[0][i] != s[0][i]) {
+            printf("%d\n",k);
+            swapPos(v, k, mod(k + 2, n));
+            k = mod(k + 2, n);
+        }
+        
+    }
+    printArray(v[0],n);
 
 }
 
+void bThreeSortOdd (int **v, int **s, int n) {
+    exit(-1);
+}
 /* Implement a ~Pythonic mod operation */
 int mod (int a, int b) {
     if(a >= 0)
@@ -75,6 +109,11 @@ int mod (int a, int b) {
     return (b+a)%b;
 }
 
+void swapPos (int **v, int x, int y) {
+   int aux = v[0][x];
+   v[0][x] = v[0][y];
+   v[0][y] = aux;
+}
 /* Create an Array of integers x , x belongs to random numbers :0*/
 int* createArray (int n) {
     int i;
