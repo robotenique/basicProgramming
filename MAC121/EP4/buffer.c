33b6b538b287ed56ed7361224560e9765406dfe5
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-10-17 13:39:31 -0200

    Add Buffer and makefile to EP4

---
+++ MAC121/EP4/buffer.c
@@ -0,0 +1,75 @@
+/*
+ * @author: Juliano Garcia de Oliveira
+ * @author: Enzo Hideki Nakamura
+ *
+ * MAC0216
+ *
+ * Buffer Implementation
+ */
+#include "buffer.h"
+#include "error.h"
+#include <stdlib.h>
+
+void *emalloc(size_t size)
+{
+  void *ret = malloc(size);
+  if (!ret) {
+    printf("call to malloc failed!");
+    exit(-1);
+  }
+  return ret;
+}
+
+/* REMAINDER: The 0 represents the end of a string. Remember to use the
+ * buffer_push_back() function to push the 0 when finished reading the
+ * desired string.
+ */
+ Buffer *buffer_create() {
+     Buffer *B = emalloc(sizeof(Buffer));
+     B -> n = 1024;
+     B -> i = 0;
+     B -> data = emalloc(B -> n) ;
+     return B;
+ }
+
+ void buffer_destroy(Buffer *B) {
+     free(B -> data);
+     free(B);
+ }
+
+ void buffer_reset(Buffer *B) {
+    free(B -> data);
+    B -> n = 1024;
+    B -> i = 0;
+    B -> data = emalloc(B -> n);
+
+ }
+
+ void buffer_push_back(Buffer *B, char c){
+     int p;
+     if((B -> i) >= B -> n) {
+        char *temp = emalloc((B -> n)*2);
+        for (p = 0; p < B -> n;p++)
+            temp[p] = B->data[p];
+        free(B -> data);
+        B -> n = 2 * (B->n);
+        B -> data = temp;
+    }
+     B -> data[B->i++] = c;
+ }
+
+ int read_line(FILE *input, Buffer *B){
+    char c;
+    int count = 0;
+    buffer_reset(B);
+
+    while((c = fgetc(input)) != EOF && c != '\n') {
+        buffer_push_back(B, c);
+        count ++;
+    }
+    if (c == '\n') {
+        buffer_push_back(B, c);
+        count ++;
+    }
+    return count;
+ }
